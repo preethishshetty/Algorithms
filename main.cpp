2d0e1efcd01fdedbd7d8cc015102d77f0b7064bc
Preethish Shetty <preethish.gunner@gmail.com>
2015-09-09 13:18:59 -0700

    Algorithms
    -My test console for trying out algorithms and mods

---
+++ main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  Algorithms
+//
+//  Created by Preethish Shetty on 9/9/15.
+//  Copyright (c) 2015 Preethish Shetty. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    cout<< "Hello Moto!\n";
+    return 0;
+}
