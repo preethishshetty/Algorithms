c14e52713f3c7d53a2cf99413e2450114d94350a
preethishshetty <preethish.gunner@gmail.com>
2015-09-09 16:25:47 -0700

    Merge Sort
    Implemented Merge Sort

--- main.cpp
+++ main.cpp
@@ -8,6 +8,8 @@
 #include <iostream>
 using namespace std;
 
+
+//=======SORT ALGORITHMS=======
 /*Bubble Sort
  - Time Complexity : O(n^2)
  - Time Complexity ( optimized ) - Best Case : O(n) when already sorted
@@ -87,9 +89,75 @@ void SelectionSort(int A[],int n)
     }
 }
 
+/* Merge Sort
+ - Time Complexity : O(NLogN )  => Using Master Theorem T(n) = 2T(n/2) + O(n) 
+ - Space Complexity : O(n)
+ - Uses : Faster algorithm as compared to the other N^2 counterparts
+*/
+void Merge(int A[],int left,int mid,int right)
+{
+    int B[10];
+    for(int i=left;i<=right;i++)
+        B[i]=A[i];
+    
+    //Indices for A
+    int i=left;
+    int j=mid+1;
+    //Index for B
+    int k=left;
+
+    while(i<=mid&&j<=right)
+    {
+        if(B[i]<B[j])
+        {
+            A[k]=B[i];
+            k++;
+            i++;
+        }
+        else
+        {
+            A[k]=B[j];
+            k++;
+            j++;
+        }
+    }
+    
+    while(i<=mid)
+    {
+        A[k]=B[i];
+        k++;
+        i++;
+    }
+    
+    while(j<=right)
+    {
+        A[k]=B[j];
+        k++;
+        j++;
+    }
+
+    cout<<"\nProgress : ";
+    for(int i=0;i<10;i++)
+        cout<<A[i]<<" ";
+}
+
+
+void MergeSort(int A[],int left,int right)
+{
+    if(left<right)
+    {
+        int mid=(right+left)/2;
+        MergeSort(A,left,mid);
+        MergeSort(A,mid+1,right);
+        Merge(A,left,mid,right);
+    }
+}
+
+//=======SORT ALGORITHMS=======
+
 int main(int argc, const char * argv[]) {
     //Initializations
-    int A[]={5,12,3,6,2,61,1,45,4,24};      //Count == 7
+    int A[]={5,12,3,6,2,61,1,45,4,24};      //Count == 10
     int i=0;
     
     //Algorithms
@@ -105,8 +173,11 @@ int main(int argc, const char * argv[]) {
     //cout<<"\nInsertion Sort : ";
     //InsertionSort(A,10);
     
-    cout<<"\nInsertion Sort : ";
-    SelectionSort(A,10);
+    //cout<<"\nSelecttion Sort : ";
+    //SelectionSort(A,10);
+    
+    cout<<"\nMerge Sort : ";
+    MergeSort(A,0,9);
     
     cout<<"\nAfter Sort -> :";
     for(i=0;i<10;i++)
